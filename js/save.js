30c7c426c51176783d108ad3ed257481eaf068c1
Fabiano Júnior <fabiano.lomonaco.junior@gmail.com>
2016-11-29 18:44:35 -0200

    Fixed Report Chart

--- js/save.js
+++ js/save.js
@@ -30,7 +30,7 @@ function init() {
             especialidade: specializationInput().val()
         };
         jQuery.ajax({
-            url: 'http://192.168.0.105:8080/doctors',
+            url: 'http://192.168.49.21:8080/doctors',
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(doctor),
